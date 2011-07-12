aa3c1c2262f677a0303c12fa34e37b9df7148d5b
Ariya Hidayat <ariya.hidayat@gmail.com>
2011-07-11 23:43:05 -0700

    Deployment helper script for Mac's PackageMaker.

---
+++ deploy/mac/setup-pkg.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+rm -rf PackageRoot
+mkdir PackageRoot
+cd PackageRoot
+mkdir eightpack
+cd eightpack
+
+mkdir bin
+cd bin
+cp ../../../../../jslint/jslint .
+cp ../../../../../jshint/jshint .
+cp ../../../../../jsbeautify/jsbeautify .
+cp ../../../../../cssbeautify/cssbeautify .
+cp ../../../../../cssmin/cssmin .
+
+strip jslint && upx -9 jslint
+strip jshint && upx -9 jshint
+strip jsbeautify && upx -9 jsbeautify
+strip cssbeautify && upx -9 cssbeautify
+strip cssmin && upx -9 cssmin
+
+cd ../../../
