28ac0ac29bb623903522d44d1ed56bcbd32754c3
James M. Greene <james.m.greene@gmail.com>
2012-05-17 21:44:48 -0500

    JamesMGreene: Added the missing commands to the deployment scripts to copy and optimize JSMin with the rest of the packages.

--- deploy/windows/setupzip.bat
+++ deploy/windows/setupzip.bat
@@ -4,12 +4,14 @@ cd eightpack
 copy ..\..\..\jslint\jslint.exe .
 copy ..\..\..\jshint\jshint.exe .
 copy ..\..\..\jsbeautify\jsbeautify.exe .
+copy ..\..\..\jsmin\jsmin.exe .
 copy ..\..\..\cssbeautify\cssbeautify.exe .
 copy ..\..\..\cssmin\cssmin.exe .
 
 strip jslint.exe && upx -9 jslint.exe
 strip jshint.exe && upx -9 jshint.exe
 strip jsbeautify.exe && upx -9 jsbeautify.exe
+strip jsmin.exe && upx -9 jsmin.exe
 strip cssbeautify.exe && upx -9 cssbeautify.exe
 strip cssmin.exe && upx -9 cssmin.exe
 
